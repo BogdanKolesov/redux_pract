24f959e01ad5ba707c979fba05711cf569ea01c1
Bogdan <bogdan_kolesov@bk.ru>
2021-09-05 11:42:56 +0600

    add: combineReducer

--- src/index.js
+++ src/index.js
@@ -3,25 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux';
+import { store } from './store/'
 
-const defaultState = {
-  cash: 5
-}
 
 //Методы store - получить состояние, изменить состояние и подписаться на изменение состояния
 
-const reducer = (state = defaultState, action) => {
-  switch (action.type) {
-    case "ADD_CASH":
-      return { ...state, cash: state.cash + action.payload }
-    case "GET_CASH":
-      return { ...state, cash: state.cash - action.payload }
-    default:
-      return state;
-  }
-}
 
-const store = createStore(reducer)
+
+
+
 
 
 ReactDOM.render(
